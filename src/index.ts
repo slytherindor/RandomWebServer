074d9b26f3c78a940f9484c0215703d214b5992f
Shubhanshu <shubh.aarsay@gmail.com>
2019-08-25 14:44:53 -0400

    Created a simple server to make pizza.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,62 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/dough', (req, res) => {
+    console.log("Dough Request Received,", new Date());
+    setTimeout(() => (
+        res.json({ dough: "gluten-free" })
+        ), randomTimeout())
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/sauce', (req, res) => {
+    console.log("Sauce Request Received,", new Date());
+    setTimeout(() => (
+        res.json({ sauce: req.query.sauce })
+        ), randomTimeout())
+});
+
+app.get('/cheese', (req, res) => {
+    console.log("Cheese Request Received,", new Date());
+    let cheese = "mozarella";
+    switch(req.query.sauce) {
+        case "red":
+            cheese = "mozarella";
+            break;
+        case "pesto":
+            cheese = "feta";
+            break;
+        case "bechamel":
+            cheese = "cheddar";
+            break;
+        case "bbq":
+            cheese = "parmesan"
+            break;
+        case "alfredo":
+            cheese = "goat"
+            break;
+        default:
+            break;
+    }
+    setTimeout(() => (
+        res.json({ cheese: cheese })
+        ), randomTimeout())
+});
+
+app.get('/chef', (req, res) => {
+    console.log("Chef Request Received,", new Date());
+    const chefs = ["Franco Pepe",
+    "Gabrielle Bonci",
+    "Ciro Salvo",
+    "Franco Caffara",
+    "Chris Bianco", 
+    "Enzo Coccia",];
+    setTimeout(() => (
+        res.json({ chef: chefs[Math.floor(Math.random() * chefs.length)] })
+        ), randomTimeout())
+});
+
+function randomTimeout() {
+    return Math.floor(Math.random() * (5000 - 1000)) + 1000;
+}
+
+app.listen(port, () => console.log(`Pizza making app listening on port ${port}!`))
